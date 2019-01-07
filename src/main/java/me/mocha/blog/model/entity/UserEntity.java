abd270abbdc5c92bf6a851bad9408a93ed4e0923
hwangseonu <hwangseonu12@naver.com>
2019-01-07 20:05:02 +0900

    fix: User entity

--- src/main/java/me/mocha/blog/model/entity/UserEntity.java
+++ src/main/java/me/mocha/blog/model/entity/UserEntity.java
@@ -2,6 +2,7 @@ package me.mocha.blog.model.entity;
 
 import lombok.*;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -14,14 +15,17 @@ import javax.persistence.Table;
 public class UserEntity {
 
     @Id
+    @Column(length = 100)
     @Getter
     @Setter
     private String username;
 
+    @Column(nullable = false)
     @Getter
     @Setter
     private String password;
 
+    @Column(nullable = false)
     @Getter
     @Setter
     private String nickname;
--- src/main/java/me/mocha/blog/model/repository/UserRepository.java
+++ src/main/java/me/mocha/blog/model/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package me.mocha.blog.model.repository;
 
+import me.mocha.blog.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserRepository, String> {
+public interface UserRepository extends JpaRepository<UserEntity, String> {
 }
