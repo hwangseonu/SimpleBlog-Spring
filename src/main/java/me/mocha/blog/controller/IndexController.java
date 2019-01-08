b6687aa5ebc0795f2b9399e9d0265e1033217e66
hwangseonu <hwangseonu12@naver.com>
2019-01-08 15:20:24 +0900

    feat: Add main page

--- src/main/java/me/mocha/blog/controller/IndexController.java
+++ src/main/java/me/mocha/blog/controller/IndexController.java
@@ -1,5 +1,7 @@
 package me.mocha.blog.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import me.mocha.blog.model.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,10 +10,13 @@ import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/")
+@Slf4j
 public class IndexController {
 
     @RequestMapping
     public ModelAndView index(ModelAndView mav, HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        mav.addObject("user", user);
         mav.setViewName("index");
         return mav;
     }
