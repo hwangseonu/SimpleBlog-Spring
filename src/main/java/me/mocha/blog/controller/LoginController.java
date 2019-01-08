e05b86b1d73c6a36bb45aed8eaf0ec85d1d9bd9d
hwangseonu <hwangseonu12@naver.com>
2019-01-08 17:09:41 +0900

    feat: Add login page

---
+++ src/main/java/me/mocha/blog/controller/LoginController.java
@@ -0,0 +1,21 @@
+package me.mocha.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @GetMapping
+    public ModelAndView login(ModelAndView mav, HttpSession session) {
+
+        mav.setViewName("login");
+        return mav;
+    }
+
+}
