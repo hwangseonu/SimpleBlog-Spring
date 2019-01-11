8ffeb2102037cb4d9eac139e65d85ab406e60363
hwangseonu <hwangseonu12@naver.com>
2019-01-11 19:54:18 +0900

    feat: Add logout

---
+++ src/main/java/me/mocha/blog/controller/LogoutController.java
@@ -0,0 +1,18 @@
+package me.mocha.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/logout")
+public class LogoutController {
+
+    @RequestMapping
+    public String logout(HttpSession session) {
+        session.removeAttribute("user");
+        return "redirect:/";
+    }
+
+}
